5a8112afcc4f1c14caefd71849b648e70541c886
shimamura <shimakou779@gmail.com>
2026-07-01 21:11:11 +0900

    [feat]UIManagerの作成中

---
+++ Assets/Scripts/System/GameUIManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+/// <summary>
+/// UIの表示と更新を行うクラス
+/// </summary>
+public class GameUIManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
