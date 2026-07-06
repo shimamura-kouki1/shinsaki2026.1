6ae34e0fef470facad5526169b1c50b82d92c79e
shimamura <shimakou779@gmail.com>
2026-07-06 22:43:56 +0900

    [feat]パスワードゲームのシステム＋UI完成

--- Assets/Scripts/MinGame/PasswordMingame.cs
+++ Assets/Scripts/MinGame/PasswordMingame.cs
@@ -1,36 +1,50 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PasswordMinigame : BaseMinigame
 {
-    [SerializeField] private int _targetCount = 3;
+    [SerializeField] private int _passwordLength = 3;
+    [SerializeField] private PasswordGameUI _gameUI;
 
-    private int _currentCount;
-    private Key _currentKey;
+    public override string Title => "パスワードを打ち込め！";
+
+    public override string Description => "表示されたキーを順番に入力しろ";
+
+    private int _currentIndex;
+    private readonly List<Key> _password = new();
+
+    private readonly Key[] _keys =
+    {
+        Key.A, Key.B, Key.C, Key.D, Key.E, Key.F,
+        Key.G, Key.H, Key.I, Key.J, Key.K, Key.L,
+        Key.M, Key.N, Key.O, Key.P, Key.Q, Key.R,
+        Key.S, Key.T, Key.U, Key.V, Key.W, Key.X,
+        Key.Y, Key.Z,
+    };
 
 
-    private readonly Key[] keys =
-    {Key.A,
-    Key.S,
-    Key.D,
-    Key.W,
-    Key.Space,
-    Key.LeftShift, };
 
     private void Update()
     {
-        if (Keyboard.current[_currentKey].wasPressedThisFrame)
-        {
-            _currentCount++;
+        if (!IsPlaying) return;
+        if (!Keyboard.current.anyKey.wasPressedThisFrame)
+            return;
 
-            if(_currentCount >= _targetCount)
+        if (Keyboard.current[_password[_currentIndex]].wasPressedThisFrame)
+        {
+            _currentIndex++;
+
+            UpdatePasswordText();
+
+            if (_currentIndex >= _password.Count)
             {
                 Finish(MinigameResult.Success);
             }
-            else
-            {
-                NextKey();
-            }
+        }
+        else
+        {
+            Finish(MinigameResult.Failure);
         }
     }
 
@@ -38,15 +52,44 @@ public class PasswordMinigame : BaseMinigame
     {
         base.StartGame();
 
-        _currentCount = 0;
-        NextKey();
+        _currentIndex = 0;
+
+        _gameUI.Show();
+        CreatePassword();
+        UpdatePasswordText();
     }
 
-    private void NextKey()
+    protected override void OnEndGame()
     {
-        _currentKey = keys[Random.Range(0, keys.Length)];
-        Debug.Log($"押すキー: {_currentKey}");
+        _gameUI.Hide();
     }
 
 
+    private void CreatePassword()
+    {
+        _password.Clear();
+
+        for (int i = 0; i < _passwordLength; i++)
+        {
+            _password.Add(_keys[Random.Range(0, _keys.Length)]);
+        }
+    }
+
+    private void UpdatePasswordText()
+    {
+        string text = "";
+
+        for (int i = 0; i < _password.Count; i++)
+        {
+            if (i < _currentIndex)
+            {
+                text += "● ";
+            }
+            else
+            {
+                text += $"{_password[i]} ";
+            }
+        }
+        _gameUI.UpdatePassword(text);
+    }
 }
